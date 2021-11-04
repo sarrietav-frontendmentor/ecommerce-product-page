2929c11e16359c520f6a7a71b66959179b326d54
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-04 15:36:43 -0500

    feat: create CartBasket component

---
+++ components/CartBasket.tsx
@@ -0,0 +1,19 @@
+const CartBasket = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="rounded-xl absolute w-[95%] h-72 top-20 z-10 bg-white flex flex-col justify-start shadow-lg">
+        <h2 className="font-bold text-lg m-5">Cart</h2>
+        <hr className="text-gray-300" />
+        <CartEmpty />
+      </div>
+    </div>
+  );
+};
+
+export default CartBasket;
+
+const CartEmpty = () => (
+  <div className="flex-grow flex flex-col justify-center items-center">
+    <p className="font-bold text-gray-500">Your cart is empty.</p>
+  </div>
+);
