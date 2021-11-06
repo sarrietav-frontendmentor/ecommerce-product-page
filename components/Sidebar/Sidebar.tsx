bf1ac1401f5f634620edafdf121f8ce8eb198b30
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-05 23:52:36 -0500

    chore(Sidebar): add test ids

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -10,6 +10,7 @@ export const Sidebar = ({
       <div
         className="w-full h-full fixed top-0 bg-black bg-opacity-70 z-10"
         onClick={() => setShown(false)}
+        data-testid="sidebar-overlay"
       ></div>
       <nav className="h-full w-8/12 fixed top-0 z-20 bg-white p-6">
         <CloseIcon className="mb-14" onClick={() => setShown(false)} />
@@ -58,6 +59,7 @@ const CloseIcon = ({
     xmlns="http://www.w3.org/2000/svg"
     className={className}
     onClick={onClick}
+    data-testid="close-icon"
   >
     <path
       d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
