a236056ce41242cbb9cba9f34311f5a0f3b491e3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-06 12:22:40 -0500

    chore: add test-ids

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -38,6 +38,7 @@ const MenuIcon = ({ onClick }: { onClick?: MouseEventHandler }) => (
     height="15"
     xmlns="http://www.w3.org/2000/svg"
     onClick={onClick}
+    data-testid="menu-icon"
   >
     <path
       d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z"
