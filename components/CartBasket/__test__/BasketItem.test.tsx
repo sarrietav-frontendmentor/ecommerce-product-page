228acfab9247b1e0dd1bdb735d6bb53bf9287948
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-06 00:18:49 -0500

    test(BasketItem): add tests

---
+++ components/CartBasket/__test__/BasketItem.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import { BasketItem } from '../BasketItem';
+
+describe('test BasketItem component', () => {
+  const dispatch = jest.fn();
+  const item = {
+    id: '',
+    image: '/image-product-1.jpg',
+    name: 'Autumn Limited Edition Sneakers',
+    price: 125.0,
+    quantity: 5,
+  };
+
+  beforeEach(() => render(<BasketItem dispatch={dispatch} item={item} />));
+
+  it('renders an image', () => {
+    expect(screen.getByRole("img")).toBeInTheDocument();
+  });
+
+  it('renders the name correctly', () => {
+    expect(screen.getByText(item.name)).toBeInTheDocument();
+  });
+
+  it('renders the base price correctly', () => {
+    expect(screen.getByText('$125.00')).toBeInTheDocument();
+  });
+
+  it('renders the quantity correctly', () => {
+    expect(screen.getByText('5')).toBeInTheDocument();
+  });
+
+  it('renders the total price correctly', () => {
+    expect(screen.getByText('$625.00')).toBeInTheDocument();
+  });
+});
