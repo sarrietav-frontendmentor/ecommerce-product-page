8198273a0ad074a92501184ca34b79616dee87ca
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-06 12:25:01 -0500

    test(Navbar): add tests

---
+++ components/Navbar/__test__/Navbar.test.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Navbar } from '../Navbar';
+
+describe('test Navbar component', () => {
+  const mockShowCartBasket = jest.fn();
+  const mockShowSidebar = jest.fn();
+
+  it('opens the basket when clicking the basket', () => {
+    render(
+      <Navbar
+        showCartBasket={mockShowCartBasket}
+        showSidebar={mockShowSidebar}
+        cartBasketShown={false}
+      />,
+    );
+
+    const cartIcon = screen.getByTestId('cart-icon');
+
+    fireEvent.click(cartIcon);
+
+    expect(mockShowCartBasket).toBeCalled();
+  });
+
+  it('opens the sidebar when clicking the menu', () => {
+    render(
+      <Navbar
+        showCartBasket={mockShowCartBasket}
+        showSidebar={mockShowSidebar}
+        cartBasketShown={false}
+      />,
+    );
+
+    const menuIcon = screen.getByTestId('menu-icon');
+
+    fireEvent.click(menuIcon);
+
+    expect(mockShowSidebar).toBeCalled();
+  });
+});
