0d46e79a9fe8c6c4f803c81cf005594a98c0d71b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-06 19:48:50 -0500

    test(BasketItem): the item deletes itself

--- components/CartBasket/__test__/BasketItem.test.tsx
+++ components/CartBasket/__test__/BasketItem.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BasketItem } from '../BasketItem';
 
 describe('test BasketItem component', () => {
@@ -14,7 +14,7 @@ describe('test BasketItem component', () => {
   beforeEach(() => render(<BasketItem dispatch={dispatch} item={item} />));
 
   it('renders an image', () => {
-    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByRole('img')).toBeInTheDocument();
   });
 
   it('renders the name correctly', () => {
@@ -31,5 +31,13 @@ describe('test BasketItem component', () => {
 
   it('renders the total price correctly', () => {
     expect(screen.getByText('$625.00')).toBeInTheDocument();
+  });
+
+  it('deletes itself', () => {
+    const trashIcon = screen.getByTestId('trash-icon');
+
+    fireEvent.click(trashIcon);
+
+    expect(dispatch).toBeCalled();
   });
 });
