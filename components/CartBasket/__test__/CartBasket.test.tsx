88938f23a81589e89f024f88391e4f8d7a7c41e2
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-06 20:08:06 -0500

    style(CartBasket): formatting

--- components/CartBasket/__test__/CartBasket.test.tsx
+++ components/CartBasket/__test__/CartBasket.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react"
-import { CartBasket } from "../CartBasket"
+import { render, screen } from '@testing-library/react';
+import { CartBasket } from '../CartBasket';
 
-describe("test CartBasket component", () => {
-  it("renders without items", () => {
-    render(<CartBasket />)
+describe('test CartBasket component', () => {
+  it('renders without items', () => {
+    render(<CartBasket />);
 
-    expect(screen.getByText("Your cart is empty.")).toBeInTheDocument();
-  })
-})
+    expect(screen.getByText('Your cart is empty.')).toBeInTheDocument();
+  });
+});
