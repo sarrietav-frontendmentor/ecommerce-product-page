6fbdf6cdfb6a333bd48cde525ce8300eab245e13
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-06 20:52:55 -0500

    test(index): add integration tests

--- pages/__test__/index.test.tsx
+++ pages/__test__/index.test.tsx
@@ -48,10 +48,68 @@ describe('test application', () => {
       render(<Home />);
 
       const cartIcon = screen.getByTestId('cart-icon');
-
       fireEvent.click(cartIcon);
 
       expect(screen.getByText('Your cart is empty.')).toBeInTheDocument();
+    });
+  });
+
+  describe('test adding items to the cart', () => {
+    const openCart = () => {
+      const cartIcon = screen.getByTestId('cart-icon');
+      fireEvent.click(cartIcon);
+    };
+
+    const addQuantity = (plusButton: HTMLElement, { times = 1 }) => {
+      for (let i = 0; i < times; i++) fireEvent.click(plusButton);
+    };
+
+    const postToCart = (button = screen.getByRole('button')) => {
+      fireEvent.click(button);
+    };
+
+    it("doesn't add items if the quantity is 0", () => {
+      render(<Home />);
+
+      postToCart();
+
+      openCart();
+
+      expect(screen.getByText('Your cart is empty.')).toBeInTheDocument();
+    });
+
+    it('adds one item to the cart', () => {
+      render(<Home />);
+
+      const plusButton = screen.getByTestId('plus-icon');
+      addQuantity(plusButton, { times: 1 });
+
+      postToCart();
+
+      openCart();
+
+      expect(screen.queryByText('Your cart is empty.')).not.toBeInTheDocument();
+      expect(
+        screen.getByText('Autumn Limited Edition Sneakers'),
+      ).toBeInTheDocument();
+    });
+
+    it('adds multiple items to the cart', () => {
+      render(<Home />);
+
+      const plusButton = screen.getByTestId('plus-icon');
+      const button = screen.getByRole('button');
+
+      for (let i = 0; i < 5; i++) {
+        addQuantity(plusButton, { times: 5 });
+        postToCart(button);
+      }
+
+      openCart();
+
+      expect(
+        screen.getAllByText('Autumn Limited Edition Sneakers').length,
+      ).toBe(5);
     });
   });
 });
