839c3ce1f6d25efa17a000faebbc5d0ed622289a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-07 22:02:12 -0500

    feat: make carousel responsive (#8)
    * feat: round image corners
    
    * feat: hide buttons
    
    * feat: add image list
    
    * fix(index): spacing on mobile
    
    * test(Carousel): fix tests by getting just the main image
    
    * feat(Carousel): change the image on click
    
    * fix(index): put the items closer
    
    * feat(Carousel): change styles for the selected image
    
    * test(Carousel): add thumbnail test
    
    * fix(carousel): use thumbnail src
    
    * refactor(carousel): create Carousel Thumbnail

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -4,37 +4,74 @@ import { MouseEventHandler, useState } from 'react';
 interface ImageLinkedListNode {
   before: number;
   src: string;
+  thumbnailSrc: string;
   after: number;
 }
 
 export const Carousel = () => {
   const imageLinkedList: ImageLinkedListNode[] = [
-    { before: 3, src: '/image-product-1.jpg', after: 1 },
-    { before: 0, src: '/image-product-2.jpg', after: 2 },
-    { before: 1, src: '/image-product-3.jpg', after: 3 },
-    { before: 2, src: '/image-product-4.jpg', after: 0 },
+    {
+      before: 3,
+      src: '/image-product-1.jpg',
+      thumbnailSrc: '/image-product-1-thumbnail.jpg',
+      after: 1,
+    },
+    {
+      before: 0,
+      src: '/image-product-2.jpg',
+      thumbnailSrc: '/image-product-2-thumbnail.jpg',
+      after: 2,
+    },
+    {
+      before: 1,
+      src: '/image-product-3.jpg',
+      thumbnailSrc: '/image-product-3-thumbnail.jpg',
+      after: 3,
+    },
+    {
+      before: 2,
+      src: '/image-product-4.jpg',
+      thumbnailSrc: '/image-product-4-thumbnail.jpg',
+      after: 0,
+    },
   ];
 
   const [currentImage, setCurrentImage] = useState(imageLinkedList[0]);
 
   return (
-    <div className="relative w-full h-80">
-      <Image
-        src={currentImage.src}
-        alt="Shoes"
-        layout="fill"
-        objectFit="cover"
-        priority
-      />
-      <div className="absolute top-2/4 left-4 bg-white h-10 w-10 rounded-full flex justify-center items-center">
-        <PreviousIcon
-          onClick={() => setCurrentImage(imageLinkedList[currentImage.before])}
+    <div className="flex flex-col space-y-7">
+      <div className="relative w-full h-96">
+        <Image
+          src={currentImage.src}
+          alt="Shoes"
+          layout="fill"
+          objectFit="cover"
+          priority
+          className="lg:rounded-xl"
+          data-testid="main-image"
         />
+        <div className="lg:hidden absolute top-2/4 left-4 bg-white h-10 w-10 rounded-full flex justify-center items-center">
+          <PreviousIcon
+            onClick={() =>
+              setCurrentImage(imageLinkedList[currentImage.before])
+            }
+          />
+        </div>
+        <div className="lg:hidden absolute top-2/4 right-4 bg-white h-10 w-10 rounded-full flex justify-center items-center">
+          <NextIcon
+            onClick={() => setCurrentImage(imageLinkedList[currentImage.after])}
+          />
+        </div>
       </div>
-      <div className="absolute top-2/4 right-4 bg-white h-10 w-10 rounded-full flex justify-center items-center">
-        <NextIcon
-          onClick={() => setCurrentImage(imageLinkedList[currentImage.after])}
-        />
+      <div className="hidden lg:flex space-x-6">
+        {imageLinkedList.map((image, index) => (
+          <CarouselThumbnail
+            key={index}
+            image={image}
+            currentImage={currentImage}
+            setCurrentImage={setCurrentImage}
+          />
+        ))}
       </div>
     </div>
   );
@@ -74,4 +111,34 @@ const NextIcon = ({ onClick }: { onClick?: MouseEventHandler }) => (
       fillRule="evenodd"
     />
   </svg>
+);
+
+const CarouselThumbnail = ({
+  image,
+  currentImage,
+  setCurrentImage,
+}: {
+  image: ImageLinkedListNode;
+  currentImage: ImageLinkedListNode;
+  setCurrentImage: (image: ImageLinkedListNode) => void;
+}): JSX.Element => (
+  <div
+    className={`w-20 h-20 relative rounded-xl ${
+      image.src === currentImage.src &&
+      'border-4 border-yellow-600 border-solid'
+    }`}
+  >
+    {image.src === currentImage.src && (
+      <div className="w-full h-full bg-white z-10 absolute bg-opacity-60 rounded-lg"></div>
+    )}
+    <Image
+      src={image.thumbnailSrc}
+      layout="fill"
+      objectFit="cover"
+      alt="Shoes"
+      className="rounded-lg"
+      data-testid="image-thumbnail"
+      onClick={() => setCurrentImage(image)}
+    />
+  </div>
 );
