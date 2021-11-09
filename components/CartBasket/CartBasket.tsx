547b9df143e2b2b73d5441b14b0ba35f23468633
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-09 12:49:13 -0500

    fix(CartBasket): spacing

--- components/CartBasket/CartBasket.tsx
+++ components/CartBasket/CartBasket.tsx
@@ -9,13 +9,13 @@ export const CartBasket = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="rounded-xl absolute w-[95%] lg:w-1/4 h-72 top-20 lg:right-10 z-10 bg-white flex flex-col justify-start shadow-lg">
+      <div className="rounded-xl absolute w-[95%] lg:w-1/4 h-72 top-20 lg:right-10 z-10 bg-white flex flex-col justify-start shadow-xl">
         <h2 className="font-bold text-lg m-5">Cart</h2>
         <hr className="text-gray-300" />
-        <div className="p-5 flex flex-col flex-grow last:justify-self-end">
+        <div className="p-5 flex flex-col flex-grow last:justify-self-end space-y-3">
           {state.items.length !== 0 ? (
             <>
-              <div className="max-h-28 overflow-scroll space-y-5">
+              <div className="h-28 overflow-scroll overflow-x-hidden space-y-5">
                 {state.items.map((item) => (
                   <BasketItem item={item} key={item.id} dispatch={dispatch} />
                 ))}
