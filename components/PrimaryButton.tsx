a18ad9f733cd81ca6fe11ab3fc3fbf08654aa76a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-09 21:36:20 -0500

    feat(PrimaryButton): add transition to the opacity

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -10,7 +10,7 @@ export const PrimaryButton = ({
   return (
     <button
       onClick={onClick}
-      className="bg-yellow-600 hover:opacity-60 flex justify-center items-center w-full rounded-lg py-5 space-x-3 shadow-xl"
+      className="transition-opacity bg-yellow-600 hover:opacity-60 flex justify-center items-center w-full rounded-lg py-5 space-x-3 shadow-xl"
     >
       {children}
     </button>
